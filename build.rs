79eae1a4faca7831e00dcdfec5ad8cb5e827b1c9
Peter Schmidt <peter@peterjs.com>
2016-10-19 18:13:37 +1100

    build.rs: Automate creating the C-API bridge
    So in theory only cargo build / cargo run should be needed.

--- build.rs
+++ build.rs
@@ -1,7 +1,37 @@
 use std::env;
+use std::process::Command;
+
 
 fn main() {
     let p = env::current_dir().unwrap();
     println!("cargo:rustc-link-search={}/lib", p.display());
-}
 
+    // Remove two files, then run two build scripts to recompile the C-API bridge in lib
+    let output = Command::new("rm").arg("lib/wt_rust_wrap.o").output()
+        .expect("removes wt_rust_wrap.o");
+    let output = Command::new("rm").arg("lib/libwt_rust_wrap.so").output()
+        .expect("removes libwt_rust_wrap.so");
+
+    // gcc -c -Wall -Werror -fpic wt_rust_wrap.c
+    let output = Command::new("gcc")
+        .arg("-c")
+        .arg("-Wall")
+        .arg("-Werror")
+        .arg("-fpic")
+        .arg("lib/wt_rust_wrap.c")
+        .arg("-o")
+        .arg("lib/wt_rust_wrap.o")
+        .output()
+        .expect("gcc should compile the wt_rust_wrap object file");
+
+    // gcc -shared -o libwt_rust_wrap.so wt_rust_wrap.o -l wiredtiger
+    let output = Command::new("gcc")
+        .arg("-shared")
+        .arg("-o")
+        .arg("lib/libwt_rust_wrap.so")
+        .arg("lib/wt_rust_wrap.o")
+        .arg("-l")
+        .arg("wiredtiger")
+        .output()
+        .expect("gcc should compile the libwt_rust_wrap shared object file");
+}
