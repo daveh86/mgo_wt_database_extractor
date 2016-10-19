ef19db87a8539e2c3c550de1a0d0179cd72f22b3
David Hows <howsdav@gmail.com>
2016-10-19 15:02:43 +1100

    Add further WT functions to wrapper (cursor mainly)

--- lib/wt_rust_wrap.c
+++ lib/wt_rust_wrap.c
@@ -19,3 +19,63 @@ int session_close(WT_SESSION* session, const char *config)
 {
 	return (session->close(session, config));
 }
+
+int create_table(WT_SESSION *session, const char *name, const char *config) 
+{
+	return (session->create(session, name, config));
+}
+
+int cursor_open(WT_SESSION* session, const char *uri, WT_CURSOR *to_dup, const char *config, WT_CURSOR **cursor)
+{
+	return (session->open_cursor(session, uri, to_dup, config, cursor));
+}
+
+int cursor_close(WT_CURSOR *cursor)
+{
+	return (cursor->close(cursor));
+}
+
+int cursor_get_value(WT_CURSOR *cursor, void *value)
+{
+	return (cursor->get_value(cursor, value));
+}
+
+int cursor_get_key(WT_CURSOR *cursor, void *key)
+{
+	return (cursor->get_key(cursor, key));
+}
+
+void cursor_set_value(WT_CURSOR *cursor, void *value) 
+{
+	cursor->set_value(cursor, value);
+}
+
+void cursor_set_key(WT_CURSOR *cursor, void *key)
+{
+	cursor->set_key(cursor, key);
+}
+
+int cursor_insert(WT_CURSOR *cursor)
+{
+	return (cursor->insert(cursor));
+}
+
+int cursor_next(WT_CURSOR *cursor)
+{
+	return (cursor->next(cursor));
+}
+
+int cursor_prev(WT_CURSOR *cursor)
+{
+	return (cursor->prev(cursor));
+}
+
+int cursor_search(WT_CURSOR *cursor)
+{
+	return (cursor->search(cursor));
+}
+
+int cursor_reset(WT_CURSOR *cursor)
+{
+	return (cursor->reset(cursor));
+}
