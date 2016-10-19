c36c8dd898ad482330c003c086f2e3742537bb31
Peter Schmidt <peter@peterjs.com>
2016-10-19 15:29:43 +1100

    Camel case Rust convention

--- src/main.rs
+++ src/main.rs
@@ -5,62 +5,62 @@ use std::os::raw::c_char;
 use std::ptr;
 
 
-struct wt_connection;
-struct wt_event_handler;
-struct wt_session;
-struct wt_cursor;
+struct WtConnection;
+struct WtEventHandler;
+struct WtSession;
+struct WtCursor;
 
 
 #[link(name = "wt_rust_wrap")]
 extern {
     fn conn_open(home: *const c_char,
-    errhandler: *mut wt_event_handler,
-    config: *const c_char,
-    conn: *mut *mut wt_connection) -> c_int;
+				 errhandler: *mut WtEventHandler,
+				 config: *const c_char,
+				 conn: *mut *mut WtConnection) -> c_int;
 
-    fn session_open(conn: *mut wt_connection,
-    errhandler: *mut wt_event_handler,
-    config: *const c_char,
-    conn: *mut *mut wt_session) -> c_int;
+    fn session_open(conn: *mut WtConnection,
+					errhandler: *mut WtEventHandler,
+					config: *const c_char,
+					conn: *mut *mut WtSession) -> c_int;
 
-    fn session_close(session: *mut wt_session,
-        config: *const c_char) -> c_int;
+    fn session_close(session: *mut WtSession,
+					 config: *const c_char) -> c_int;
 
-    fn conn_close(conn: *mut wt_connection,
-        config: *const c_char) -> c_int;
+    fn conn_close(conn: *mut WtConnection,
+				  config: *const c_char) -> c_int;
 
-    fn create_table(session: *mut wt_session,
-	name: *const c_char, config: *const c_char) -> c_int;
+    fn create_table(session: *mut WtSession,
+					name: *const c_char, config: *const c_char) -> c_int;
 
-    fn drop_table(session: *mut wt_session,
-        name: *const c_char, config: *const c_char) -> c_int;
+    fn drop_table(session: *mut WtSession,
+				  name: *const c_char, config: *const c_char) -> c_int;
 
-    fn cursor_open(session: *mut wt_session,
-	uri: *const c_char, to_dup: *mut wt_cursor, config : *const c_char,
-	cursor: *mut *mut wt_cursor) -> c_int;
+    fn cursor_open(session: *mut WtSession,
+				   uri: *const c_char, to_dup: *mut WtCursor, config : *const c_char,
+				   cursor: *mut *mut WtCursor) -> c_int;
 
-    fn cusror_close(cursor: *mut wt_cursor) -> c_int;
+    fn cusror_close(cursor: *mut WtCursor) -> c_int;
 
     // Cursor data manip
-    fn cursor_get_value(cursor: *mut wt_cursor, value: *mut c_void) -> c_int;
-    fn cursor_get_key(cursor: *mut wt_cursor, key: *mut c_void) -> c_int;
-    fn cursor_set_value(cursor: *mut wt_cursor, value: *mut c_void) -> ();
-    fn cursor_set_key(cursor: *mut wt_cursor, key: *mut c_void) -> ();
+    fn cursor_get_value(cursor: *mut WtCursor, value: *mut c_void) -> c_int;
+    fn cursor_get_key(cursor: *mut WtCursor, key: *mut c_void) -> c_int;
+    fn cursor_set_value(cursor: *mut WtCursor, value: *mut c_void) -> ();
+    fn cursor_set_key(cursor: *mut WtCursor, key: *mut c_void) -> ();
 
     // Cursor actions
-    fn cursor_insert(cursor: *mut wt_cursor) -> c_int;
-    fn cursor_next(cursor: *mut wt_cursor) -> c_int;
-    fn cursor_perv(cursor: *mut wt_cursor) -> c_int;
-    fn cursor_search(cursor: *mut wt_cursor) -> c_int;
-    fn cursor_reset(cursor: *mut wt_cursor) -> c_int;
+    fn cursor_insert(cursor: *mut WtCursor) -> c_int;
+    fn cursor_next(cursor: *mut WtCursor) -> c_int;
+    fn cursor_perv(cursor: *mut WtCursor) -> c_int;
+    fn cursor_search(cursor: *mut WtCursor) -> c_int;
+    fn cursor_reset(cursor: *mut WtCursor) -> c_int;
 }
 
 fn main() {
 	// WT_CONN*
-	let mut conn: *mut wt_connection = ptr::null_mut();
+	let mut conn: *mut WtConnection = ptr::null_mut();
 
 	// WT_SESSION*
-	let mut session: *mut wt_session = ptr::null_mut();
+	let mut session: *mut WtSession = ptr::null_mut();
 
 	// Variables
 	let home = CString::new("WT_TEST").unwrap();
