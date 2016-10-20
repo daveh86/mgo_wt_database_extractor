5ea97c76e6c640b8dad1038dafa540ddf12ce7ae
David Hows <howsdav@gmail.com>
2016-10-20 14:20:31 +1100

    First function!

--- src/main.rs
+++ src/main.rs
@@ -62,74 +62,98 @@ extern {
 //    fn cursor_reset(cursor: *mut WtCursor) -> c_int;
 }
 
+fn list_tables(session: *mut WtSession) -> () {
+
+        // WT_CURSOR*
+        let mut cursor: *mut WtCursor = ptr::null_mut();
+
+        // Variables
+        let table_name = CString::new("table:_mdb_catalog").unwrap();
+
+        //let table_conf = CString::new("key_format=q,value_format=S").unwrap();
+        //let mut x: i64 = 123;
+        //let x_raw = &mut x as *mut i64;
+        let mut refetched_key: i64 = 0;
+        let mut refetched_value: *mut u8 = ptr::null_mut();
+        let mut refetched_len: usize = 0;
+        unsafe {
+                // TODO: Error handling? https://doc.rust-lang.org/book/error-handling.html
+                let ret = cursor_open(session,
+                        table_name.as_ptr(),
+                        ptr::null_mut(),
+                        ptr::null(),
+                        &mut cursor);
+
+                if ret != 0 {
+                        println!("Error. opening connection!");
+                        return();
+                }	
+                let ret = cursor_open(session,
+                        table_name.as_ptr(),
+                        ptr::null_mut(),
+                        ptr::null(),
+                        &mut cursor);
+
+                if ret != 0 {
+                        println!("Error. opening connection!");
+                        return();
+                }
+                while cursor_next(cursor) == 0 {
+                        let ret = cursor_get_key_i64(cursor, &mut refetched_key);
+			if ret != 0 {
+				continue;
+			}
+                        cursor_get_value_item(cursor, &mut refetched_value, &mut refetched_len);
+                        let slicey = slice::from_raw_parts(refetched_value, refetched_len);
+                        let doc = decode_document(&mut Cursor::new(slicey.to_vec())).unwrap();
+                        let file = doc.get("ident");
+                        if file != None {
+                                let ns = doc.get("ns").unwrap();
+                                println!("collection {} is file {}", ns, file.unwrap());
+                                if doc.get("idxIdent") != None {
+                                        let idxdoc = doc.get("idxIdent").unwrap();
+					println!("indexes:");
+                                        for (k,v) in idxdoc.to_json().as_object().unwrap() {
+                                                println!("\t{} : {}", k, v);
+                                        }
+                                }
+				println!("")
+                        }
+                }
+                cursor_close(cursor);
+	}
+}
+
 fn main() {
 	// WT_CONN*
 	let mut conn: *mut WtConnection = ptr::null_mut();
-
 	// WT_SESSION*
-	let mut session: *mut WtSession = ptr::null_mut();
-
-	// WT_CURSOR*
-	let mut cursor: *mut WtCursor = ptr::null_mut();
-
-	// Variables
+        let mut session: *mut WtSession = ptr::null_mut();
+	
 	let db_path = CString::new("/data/db").unwrap();
-	let db_conf = CString::new("create,statistics=(fast)").unwrap();
-	let table_name = CString::new("table:_mdb_catalog").unwrap();
-
-	//let table_conf = CString::new("key_format=q,value_format=S").unwrap();
-	//let mut x: i64 = 123;
-	//let x_raw = &mut x as *mut i64;
-	let mut refetched_key: i64 = 0;
-	let mut refetched_value: *mut u8 = ptr::null_mut();
-	let mut refetched_len: usize = 0;
+        let db_conf = CString::new("create,statistics=(fast)").unwrap();
 
 	unsafe {
-		// TODO: Error handling? https://doc.rust-lang.org/book/error-handling.html
 		let ret = conn_open(
-			db_path.as_ptr(),
-			ptr::null_mut(),
-			db_conf.as_ptr(),
-			&mut conn);
-
-		if ret != 0 {
-			println!("Error. opening connection!");
-			return();
-		}
-
-		let ret = session_open(conn,
-			ptr::null_mut(),
-			ptr::null_mut(),
-			&mut session);
-
-		if ret != 0 {
+                        db_path.as_ptr(),
+                        ptr::null_mut(),
+                        db_conf.as_ptr(),
+                        &mut conn);
+                if ret != 0 {
                         println!("Error. opening connection!");
                         return();
-		}
+                }
 
-		let ret = cursor_open(session,
-			table_name.as_ptr(),
-			ptr::null_mut(),
-			ptr::null(),
-			&mut cursor);
+                let ret = session_open(conn,
+                        ptr::null_mut(),
+                        ptr::null_mut(),
+                        &mut session);
 
-		if ret != 0 {
+                if ret != 0 {
                         println!("Error. opening connection!");
                         return();
-		}
-		let mut counter = 0;
-		while cursor_next(cursor) == 0 {
-			let ret = cursor_get_key_i64(cursor, &mut refetched_key);
-			println!("get key returned: {}", ret);
-			println!("key is: {}", refetched_key);
-			cursor_get_value_item(cursor, &mut refetched_value, &mut refetched_len);
-			let slicey = slice::from_raw_parts(refetched_value, refetched_len);
-			let doc = decode_document(&mut Cursor::new(slicey.to_vec())).unwrap();
-			println!("Decoded: {:?}", doc);
-			counter+=1;
-			println!("entry number: {}", counter);
-		}
-		cursor_close(cursor);
+                }
+		list_tables(session);
 		session_close(session, ptr::null_mut());
 		conn_close(conn, ptr::null_mut());
 	}
