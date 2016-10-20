2cff11ffaa07b14171c8fd5c29bf0acaa84906f3
David Hows <howsdav@gmail.com>
2016-10-20 15:02:16 +1100

    Fix whitespace, switch from tab to 4s

--- src/main.rs
+++ src/main.rs
@@ -21,30 +21,30 @@ enum WtCursor {}
 #[link(name = "wt_rust_wrap")]
 extern {
     fn conn_open(home: *const c_char,
-				 errhandler: *mut WtEventHandler,
-				 config: *const c_char,
-				 conn: *mut *mut WtConnection) -> c_int;
+                 errhandler: *mut WtEventHandler,
+                 config: *const c_char,
+                 conn: *mut *mut WtConnection) -> c_int;
 
     fn session_open(conn: *mut WtConnection,
-					errhandler: *mut WtEventHandler,
-					config: *const c_char,
-					conn: *mut *mut WtSession) -> c_int;
+                    errhandler: *mut WtEventHandler,
+                    config: *const c_char,
+                    conn: *mut *mut WtSession) -> c_int;
 
     fn session_close(session: *mut WtSession,
-					 config: *const c_char) -> c_int;
+                     config: *const c_char) -> c_int;
 
     fn conn_close(conn: *mut WtConnection,
-				  config: *const c_char) -> c_int;
+                  config: *const c_char) -> c_int;
 /*
     fn create_table(session: *mut WtSession,
-					name: *const c_char, config: *const c_char) -> c_int;
+                    name: *const c_char, config: *const c_char) -> c_int;
 
     fn drop_table(session: *mut WtSession,
-				  name: *const c_char, config: *const c_char) -> c_int;
+                  name: *const c_char, config: *const c_char) -> c_int;
 */
     fn cursor_open(session: *mut WtSession,
-				   uri: *const c_char, to_dup: *mut WtCursor, config : *const c_char,
-				   cursor: *mut *mut WtCursor) -> c_int;
+                   uri: *const c_char, to_dup: *mut WtCursor, config : *const c_char,
+                   cursor: *mut *mut WtCursor) -> c_int;
 
     fn cursor_close(cursor: *mut WtCursor) -> c_int;
 
@@ -67,53 +67,53 @@ extern {
 
 fn list_tables(session: *mut WtSession) -> () {
 
-        // WT_CURSOR*
-        let mut cursor: *mut WtCursor = ptr::null_mut();
+    // WT_CURSOR*
+    let mut cursor: *mut WtCursor = ptr::null_mut();
 
-        // Variables
-        let table_name = CString::new("table:_mdb_catalog").unwrap();
+    // Variables
+    let table_name = CString::new("table:_mdb_catalog").unwrap();
 
-        //let mut x: i64 = 123;
-        //let x_raw = &mut x as *mut i64;
-        let mut refetched_key: i64 = 0;
-        let mut refetched_value: *mut u8 = ptr::null_mut();
-        let mut refetched_len: usize = 0;
-        unsafe {
-                // TODO: Error handling? https://doc.rust-lang.org/book/error-handling.html
-                let ret = cursor_open(session,
-                        table_name.as_ptr(),
-                        ptr::null_mut(),
-                        ptr::null(),
-                        &mut cursor);
+    //let mut x: i64 = 123;
+    //let x_raw = &mut x as *mut i64;
+    let mut refetched_key: i64 = 0;
+    let mut refetched_value: *mut u8 = ptr::null_mut();
+    let mut refetched_len: usize = 0;
+    unsafe {
+        // TODO: Error handling? https://doc.rust-lang.org/book/error-handling.html
+        let ret = cursor_open(session,
+            table_name.as_ptr(),
+            ptr::null_mut(),
+            ptr::null(),
+            &mut cursor);
 
-                if ret != 0 {
-                        println!("Error. opening connection!");
-                        return();
+        if ret != 0 {
+            println!("Error. opening connection!");
+            return();
+        }
+        while cursor_next(cursor) == 0 {
+            let ret = cursor_get_key_i64(cursor, &mut refetched_key);
+            if ret != 0 {
+                continue;
+            }
+            cursor_get_value_item(cursor, &mut refetched_value, &mut refetched_len);
+            let slicey = slice::from_raw_parts(refetched_value, refetched_len);
+            let doc = decode_document(&mut Cursor::new(slicey.to_vec())).unwrap();
+            let file = doc.get("ident");
+            if file != None {
+                let ns = doc.get("ns").unwrap();
+                println!("collection {} is file {}", ns, file.unwrap());
+                if doc.get("idxIdent") != None {
+                    let idxdoc = doc.get("idxIdent").unwrap();
+                    println!("indexes:");
+                    for (k,v) in idxdoc.to_json().as_object().unwrap() {
+                        println!("\t{} : {}", k, v);
+                    }
                 }
-                while cursor_next(cursor) == 0 {
-                        let ret = cursor_get_key_i64(cursor, &mut refetched_key);
-			if ret != 0 {
-				continue;
-			}
-                        cursor_get_value_item(cursor, &mut refetched_value, &mut refetched_len);
-                        let slicey = slice::from_raw_parts(refetched_value, refetched_len);
-                        let doc = decode_document(&mut Cursor::new(slicey.to_vec())).unwrap();
-                        let file = doc.get("ident");
-                        if file != None {
-                                let ns = doc.get("ns").unwrap();
-                                println!("collection {} is file {}", ns, file.unwrap());
-                                if doc.get("idxIdent") != None {
-                                        let idxdoc = doc.get("idxIdent").unwrap();
-					println!("indexes:");
-                                        for (k,v) in idxdoc.to_json().as_object().unwrap() {
-                                                println!("\t{} : {}", k, v);
-                                        }
-                                }
-				println!("")
-                        }
-                }
-                cursor_close(cursor);
-	}
+                println!("")
+            }
+        }
+        cursor_close(cursor);
+    }
 }
 
 fn print_usage(program: &str, opts: Options) {
@@ -122,73 +122,73 @@ fn print_usage(program: &str, opts: Options) {
 }
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	let program = args[0].clone();
-	let mut opts = Options::new();
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut opts = Options::new();
 
-	opts.optopt("d", "dbpath", "set dbpath to read from (Defaults to /data/db)", "DBPATH");
-	opts.optopt("o", "outpath", "set dbpath to write to", "OUTPATH");
-	opts.optopt("t", "tables", "list of tables to be copied", "TABLES");
-	opts.optflag("l", "list", "list the table mappings");
-	opts.optflag("h", "help", "print this help menu");
+    opts.optopt("d", "dbpath", "set dbpath to read from (Defaults to /data/db)", "DBPATH");
+    opts.optopt("o", "outpath", "set dbpath to write to", "OUTPATH");
+    opts.optopt("t", "tables", "list of tables to be copied", "TABLES");
+    opts.optflag("l", "list", "list the table mappings");
+    opts.optflag("h", "help", "print this help menu");
 
-	let matches = match opts.parse(&args[1..]) {
-		Ok(m) => { m }
-		Err(f) => { panic!(f.to_string()) }
-	};
-	if matches.opt_present("h") {
-		print_usage(&program, opts);
-		return;
-	}
-		
-	let db_path = match matches.opt_str("d") {
-		Some(s) => CString::new(s).unwrap(),
-		None => CString::new("/data/db").unwrap(),
-	};
-	let out_path = matches.opt_str("o");
-	let tables = matches.opt_str("t");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
 
-	// WT_CONN*
-	let mut conn: *mut WtConnection = ptr::null_mut();
-	// WT_SESSION*
-        let mut session: *mut WtSession = ptr::null_mut();
-        let db_conf = CString::new("create,statistics=(fast)").unwrap();
+    let db_path = match matches.opt_str("d") {
+        Some(s) => CString::new(s).unwrap(),
+        None => CString::new("/data/db").unwrap(),
+    };
+    let out_path = matches.opt_str("o");
+    let tables = matches.opt_str("t");
 
-	unsafe {
-		let ret = conn_open(
-                        db_path.as_ptr(),
-                        ptr::null_mut(),
-                        db_conf.as_ptr(),
-                        &mut conn);
-                if ret != 0 {
-                        println!("Error opening connection!");
-                        return();
-                }
+    // WT_CONN*
+    let mut conn: *mut WtConnection = ptr::null_mut();
+    // WT_SESSION*
+    let mut session: *mut WtSession = ptr::null_mut();
+    let db_conf = CString::new("create,statistics=(fast)").unwrap();
 
-                let ret = session_open(conn,
-                        ptr::null_mut(),
-                        ptr::null_mut(),
-                        &mut session);
+    unsafe {
+        let ret = conn_open(
+            db_path.as_ptr(),
+            ptr::null_mut(),
+            db_conf.as_ptr(),
+            &mut conn);
+        if ret != 0 {
+            println!("Error opening connection!");
+            return();
+        }
 
-                if ret != 0 {
-                        println!("Error opening session!");
-                        return();
-                }
-		if matches.opt_present("l") {
-			list_tables(session);
-		} else {
-			if out_path == None { 
-				println!("No Outpath set!");
-				return();
-			}
-			if tables == None {
-				panic!("No tables listed");
-				return();
-			}
-			println!("do something else");
-		}
-		session_close(session, ptr::null_mut());
-		conn_close(conn, ptr::null_mut());
-	}
+        let ret = session_open(conn,
+            ptr::null_mut(),
+            ptr::null_mut(),
+            &mut session);
+
+        if ret != 0 {
+            println!("Error opening session!");
+            return();
+        }
+        if matches.opt_present("l") {
+            list_tables(session);
+        } else {
+            if out_path == None {
+                println!("No Outpath set!");
+                return();
+            }
+            if tables == None {
+                panic!("No tables listed");
+                return();
+            }
+            println!("do something else");
+        }
+        session_close(session, ptr::null_mut());
+        conn_close(conn, ptr::null_mut());
+    }
 }
 
